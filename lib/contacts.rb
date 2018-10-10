319894fa2c366e3fd4b504cb4b7f74c7362fdb01
Kevin J Gomez <kjgmez1@gmail.com>
2018-10-10 01:39:52 +0000

    automatically backed up by learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,6 +15,6 @@ require 'pry'
   # }
 
 def remove_strawberry(contacts)
-  contacts["Freddy Mercury"][favorite_ice_cream_flavors].each{|x| x.clear if value == "strawberry"}
+  contacts["Freddy Mercury"][:favorite_ice_cream_flavors].each{|x| x.clear if value == "strawberry"}
   contacts
 end
